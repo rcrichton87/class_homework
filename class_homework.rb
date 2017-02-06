6db1858d2cd2ca4bbc5014057abfe78117b78147
Ross Crichton <rcrichton87@googlemail.com>
2017-02-06 14:24:13 +0000

    team_name method added

--- class_homework.rb
+++ class_homework.rb
@@ -1,34 +1,50 @@
-class CodeClanStudent
-
 #Part A
 
-  # def initialize(student_name, cohort)
-  #   @student_name = student_name
-  #   @cohort = cohort
-  # end
+# class CodeClanStudent
 
-  # def student_name
-  #   return @student_name
-  # end 
+#   def initialize(student_name, cohort)
+#     @student_name = student_name
+#     @cohort = cohort
+#   end
 
-  # def cohort
-  #   return @cohort
-  # end
+#   def student_name
+#     return @student_name
+#   end 
 
-  # def set_name(new_name)
-  #   @student_name = new_name
-  # end
+#   def cohort
+#     return @cohort
+#   end
 
-  # def set_cohort(new_cohort)
-  #   @cohort = new_cohort
-  # end
+#   def set_name(new_name)
+#     @student_name = new_name
+#   end
 
-  # def talk
-  #   return "I can talk!"
-  # end
+#   def set_cohort(new_cohort)
+#     @cohort = new_cohort
+#   end
 
-  # def favourite_language(language)
-  #   return "I love #{language}"
-  # end
+#   def talk
+#     return "I can talk!"
+#   end
+
+#   def favourite_language(language)
+#     return "I love #{language}"
+#   end
+
+# end
+
+#Part B
+
+class SportsTeam
+
+  def initialize(team_name, players, coach)
+    @team_name = team_name
+    @players = players
+    @coach = coach
+  end
+
+  def team_name
+    return @team_name
+  end
 
 end
--- class_homework_specs.rb
+++ class_homework_specs.rb
@@ -1,40 +1,54 @@
 require ('minitest/autorun')
 require_relative('./class_homework')
 
-class TestCodeClanStudent < MiniTest::Test
-
 #Part A
 
-  # def test_student_name
-  #   student = CodeClanStudent.new("Ross", "Cohort 10")
-  #   assert_equal("Ross", student.student_name)
-  # end
+# class TestCodeClanStudent < MiniTest::Test
 
-  # def test_cohort
-  #   student = CodeClanStudent.new("Ross", "Cohort 10")
-  #   assert_equal("Cohort 10", student.cohort)
-  # end
+#   def test_student_name
+#     student = CodeClanStudent.new("Ross", "Cohort 10")
+#     assert_equal("Ross", student.student_name)
+#   end
 
-  # def test_set_name
-  #   student = CodeClanStudent.new("Ross", "Cohort 10")
-  #   student.set_name("David")
-  #   assert_equal("David", student.student_name)
-  # end
+#   def test_cohort
+#     student = CodeClanStudent.new("Ross", "Cohort 10")
+#     assert_equal("Cohort 10", student.cohort)
+#   end
 
-  # def test_set_cohort
-  #   student = CodeClanStudent.new("Ross", "Cohort 10")
-  #   student.set_cohort("Cohort 8")
-  #   assert_equal("Cohort 8", student.cohort)
-  # end
+#   def test_set_name
+#     student = CodeClanStudent.new("Ross", "Cohort 10")
+#     student.set_name("David")
+#     assert_equal("David", student.student_name)
+#   end
 
-  # def test_talk
-  #   student = CodeClanStudent.new("Ross", "Cohort 10")
-  #   assert_equal("I can talk!", student.talk)
-  # end
+#   def test_set_cohort
+#     student = CodeClanStudent.new("Ross", "Cohort 10")
+#     student.set_cohort("Cohort 8")
+#     assert_equal("Cohort 8", student.cohort)
+#   end
 
-  # def test_favourite_language
-  #   student = CodeClanStudent.new("Ross", "Cohort 10")
-  #   assert_equal("I love Ruby", student.favourite_language("Ruby"))
-  # end
+#   def test_talk
+#     student = CodeClanStudent.new("Ross", "Cohort 10")
+#     assert_equal("I can talk!", student.talk)
+#   end
+
+#   def test_favourite_language
+#     student = CodeClanStudent.new("Ross", "Cohort 10")
+#     assert_equal("I love Ruby", student.favourite_language("Ruby"))
+#   end
+
+# end
+
+#Part B
+
+class TestSportsTeam < MiniTest::Test
+
+  def test_team_name
+    team = SportsTeam.new("Handegg Handlers", ["Hack Enslash", "Mad Donna"], "Amanda Hugandkiss")
+    assert_equal("Handegg Handlers", team.team_name)
+  end
 
 end
+
+
+
