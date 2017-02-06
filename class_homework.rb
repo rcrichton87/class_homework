62de379510b90904ef4c8e71c3c070be50855227
Ross Crichton <rcrichton87@googlemail.com>
2017-02-06 14:11:22 +0000

    favourite_language method method added

--- class_homework.rb
+++ class_homework.rb
@@ -1,27 +1,34 @@
 class CodeClanStudent
 
-  def initialize(student_name, cohort)
-    @student_name = student_name
-    @cohort = cohort
-  end
+#Part A
 
-  def student_name
-    return @student_name
-  end 
+  # def initialize(student_name, cohort)
+  #   @student_name = student_name
+  #   @cohort = cohort
+  # end
 
-  def cohort
-    return @cohort
-  end
+  # def student_name
+  #   return @student_name
+  # end 
 
-  def set_name(new_name)
-    @student_name = new_name
-  end
+  # def cohort
+  #   return @cohort
+  # end
 
-  def set_cohort(new_cohort)
-    @cohort = new_cohort
-  end
+  # def set_name(new_name)
+  #   @student_name = new_name
+  # end
 
-  def talk
-    return "I can talk!"
-  end
+  # def set_cohort(new_cohort)
+  #   @cohort = new_cohort
+  # end
+
+  # def talk
+  #   return "I can talk!"
+  # end
+
+  # def favourite_language(language)
+  #   return "I love #{language}"
+  # end
+
 end
--- class_homework_specs.rb
+++ class_homework_specs.rb
@@ -3,31 +3,38 @@ require_relative('./class_homework')
 
 class TestCodeClanStudent < MiniTest::Test
 
-  def test_student_name
-    student = CodeClanStudent.new("Ross", "Cohort 10")
-    assert_equal("Ross", student.student_name)
-  end
+#Part A
 
-  def test_cohort
-    student = CodeClanStudent.new("Ross", "Cohort 10")
-    assert_equal("Cohort 10", student.cohort)
-  end
+  # def test_student_name
+  #   student = CodeClanStudent.new("Ross", "Cohort 10")
+  #   assert_equal("Ross", student.student_name)
+  # end
 
-  def test_set_name
-    student = CodeClanStudent.new("Ross", "Cohort 10")
-    student.set_name("David")
-    assert_equal("David", student.student_name)
-  end
+  # def test_cohort
+  #   student = CodeClanStudent.new("Ross", "Cohort 10")
+  #   assert_equal("Cohort 10", student.cohort)
+  # end
 
-  def test_set_cohort
-    student = CodeClanStudent.new("Ross", "Cohort 10")
-    student.set_cohort("Cohort 8")
-    assert_equal("Cohort 8", student.cohort)
-  end
+  # def test_set_name
+  #   student = CodeClanStudent.new("Ross", "Cohort 10")
+  #   student.set_name("David")
+  #   assert_equal("David", student.student_name)
+  # end
 
-  def test_talk
-    student = CodeClanStudent.new("Ross", "Cohort 10")
-    assert_equal("I can talk!", student.talk)
-  end
-  
+  # def test_set_cohort
+  #   student = CodeClanStudent.new("Ross", "Cohort 10")
+  #   student.set_cohort("Cohort 8")
+  #   assert_equal("Cohort 8", student.cohort)
+  # end
+
+  # def test_talk
+  #   student = CodeClanStudent.new("Ross", "Cohort 10")
+  #   assert_equal("I can talk!", student.talk)
+  # end
+
+  # def test_favourite_language
+  #   student = CodeClanStudent.new("Ross", "Cohort 10")
+  #   assert_equal("I love Ruby", student.favourite_language("Ruby"))
+  # end
+
 end
